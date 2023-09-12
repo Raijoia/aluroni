4fb9b5a65afe6c33d0a9b16862e1adb57e94ae40
Raí <megac154@gmail.com>
2023-09-12 14:19:45 -0300

    notfound page when page dont exist

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,12 +2,13 @@ import styles from './Prato.module.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
 
 export default function Prato() {
   const { id } = useParams();
   const prato = cardapio.find(item => item.id === Number(id));
   if(!prato) {
-    return '';
+    return <NotFound />;
   }
 
   const navigate = useNavigate();
